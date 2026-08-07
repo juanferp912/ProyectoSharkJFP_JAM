b7bf424aa28ba41d4b87fdec8bc04d671788e531
juanferp912 <jpastor@uees.edu.ec>
2026-08-07 09:47:28 -0500

    mejora fisica del arpon

--- Assets/Scripts/Enemies/HarpoonProjectile.cs
+++ Assets/Scripts/Enemies/HarpoonProjectile.cs
@@ -7,108 +7,158 @@ public class HarpoonProjectile : MonoBehaviour
     [SerializeField] private float tiempoDeVida = 5f;
     [SerializeField] private float correccionRotacion;
 
-    [Header("Agua y aire")]
+    [Header("Aire")]
     [SerializeField] private float nivelSuperficieAgua = 8f;
     [SerializeField] private float gravedadAire = 8f;
-    [SerializeField] private float velocidadMaximaCaida = 12f;
+    [SerializeField] private float velocidadMaximaCaidaAire = 12f;
 
-    private Vector2 direccion;
-    private float velocidadVerticalAire;
-    private bool enAire;
+    [Header("Regreso al agua")]
+    [SerializeField] private float factorFrenadoEntradaAgua = 0.3f;
+    [SerializeField] private float gravedadBajoAgua = 0.8f;
+    [SerializeField] private float velocidadMaximaCaidaAgua = 2.5f;
+
+    private Vector2 velocidadActual;
+    private bool direccionConfigurada;
+    private bool salioDelAgua;
+    private bool volvioAlAgua;
 
     private void Start()
     {
-        Destroy(gameObject, tiempoDeVida);
+        Destroy(
+            gameObject,
+            tiempoDeVida
+        );
     }
 
-    public void ConfigurarDireccion(Vector2 nuevaDireccion)
+    public void ConfigurarDireccion(
+        Vector2 nuevaDireccion
+    )
     {
-        direccion = nuevaDireccion.normalized;
+        velocidadActual =
+            nuevaDireccion.normalized *
+            velocidad;
 
-        float angulo =
-            Mathf.Atan2(direccion.y, direccion.x) *
-            Mathf.Rad2Deg;
+        direccionConfigurada = true;
 
-        transform.rotation = Quaternion.Euler(
-            0f,
-            0f,
-            angulo + correccionRotacion
-        );
+        ActualizarRotacion();
     }
 
     private void Update()
     {
-        if (transform.position.y > nivelSuperficieAgua)
+        if (!direccionConfigurada)
+        {
+            return;
+        }
+
+        RevisarEstado();
+
+        if (volvioAlAgua)
+        {
+            MoverDespuesDeEntrarAlAgua();
+        }
+        else if (salioDelAgua)
         {
             MoverEnAire();
         }
-        else
-        {
-            MoverEnAgua();
-        }
-    }
-
-    private void MoverEnAgua()
-    {
-        enAire = false;
-        velocidadVerticalAire = 0f;
 
         transform.position +=
-            (Vector3)(direccion * velocidad * Time.deltaTime);
+            (Vector3)(
+                velocidadActual *
+                Time.deltaTime
+            );
+
+        ActualizarRotacion();
+    }
+
+    private void RevisarEstado()
+    {
+        if (
+            !salioDelAgua &&
+            transform.position.y >
+            nivelSuperficieAgua
+        )
+        {
+            salioDelAgua = true;
+        }
+
+        if (
+            salioDelAgua &&
+            !volvioAlAgua &&
+            transform.position.y <=
+            nivelSuperficieAgua &&
+            velocidadActual.y < 0f
+        )
+        {
+            volvioAlAgua = true;
+
+            velocidadActual *=
+                factorFrenadoEntradaAgua;
+        }
     }
 
     private void MoverEnAire()
     {
-        if (!enAire)
-        {
-            enAire = true;
+        velocidadActual.y -=
+            gravedadAire *
+            Time.deltaTime;
 
-            velocidadVerticalAire =
-                direccion.y * velocidad;
-        }
-
-        velocidadVerticalAire -=
-            gravedadAire * Time.deltaTime;
-
-        velocidadVerticalAire = Mathf.Max(
-            velocidadVerticalAire,
-            -velocidadMaximaCaida
-        );
-
-        float movimientoX =
-            direccion.x * velocidad * Time.deltaTime;
-
-        float movimientoY =
-            velocidadVerticalAire * Time.deltaTime;
-
-        transform.position += new Vector3(
-            movimientoX,
-            movimientoY,
-            0f
-        );
-
-        Vector2 direccionVisual = new Vector2(
-            direccion.x * velocidad,
-            velocidadVerticalAire
-        ).normalized;
-
-        if (direccionVisual.sqrMagnitude > 0.01f)
-        {
-            float angulo =
-                Mathf.Atan2(
-                    direccionVisual.y,
-                    direccionVisual.x
-                ) * Mathf.Rad2Deg;
-
-            transform.rotation = Quaternion.Euler(
-                0f,
-                0f,
-                angulo + correccionRotacion
+        velocidadActual.y =
+            Mathf.Max(
+                velocidadActual.y,
+                -velocidadMaximaCaidaAire
             );
-        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void MoverDespuesDeEntrarAlAgua()
+    {
+        velocidadActual.y -=
+            gravedadBajoAgua *
+            Time.deltaTime;
+
+        velocidadActual.y =
+            Mathf.Max(
+                velocidadActual.y,
+                -velocidadMaximaCaidaAgua
+            );
+
+        velocidadActual.x =
+            Mathf.MoveTowards(
+                velocidadActual.x,
+                0f,
+                1.5f *
+                Time.deltaTime
+            );
+    }
+
+    private void ActualizarRotacion()
+    {
+        if (
+            velocidadActual.sqrMagnitude <=
+            0.01f
+        )
+        {
+            return;
+        }
+
+        float angulo =
+            Mathf.Atan2(
+                velocidadActual.y,
+                velocidadActual.x
+            ) *
+            Mathf.Rad2Deg;
+
+        transform.rotation =
+            Quaternion.Euler(
+                0f,
+                0f,
+                angulo +
+                correccionRotacion
+            );
+    }
+
+    private void OnTriggerEnter2D(
+        Collider2D collision
+    )
     {
         if (!collision.CompareTag("Player"))
         {
@@ -117,6 +167,12 @@ public class HarpoonProjectile : MonoBehaviour
 
         PlayerHealth saludJugador =
             collision.GetComponent<PlayerHealth>();
+
+        if (saludJugador == null)
+        {
+            saludJugador =
+                collision.GetComponentInParent<PlayerHealth>();
+        }
 
         if (saludJugador != null)
         {
